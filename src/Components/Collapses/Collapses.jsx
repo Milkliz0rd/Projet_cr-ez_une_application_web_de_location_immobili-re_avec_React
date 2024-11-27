a0144da40bb491bdee2cd116c9adfb57129304c0
Morozoff Alexandre <morozoff_alexandre@outlook.com>
2024-11-27 19:15:27 +0100

    adding the first transition

--- src/Components/Collapses/Collapses.jsx
+++ src/Components/Collapses/Collapses.jsx
@@ -1,16 +1,26 @@
 import { faChevronUp } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
+import React, { useState } from 'react'
 
 const Collapses = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  function toggleCollapse() {
+    setIsOpen(!isOpen)
+  }
   return (
     <div className="collapse-container">
       <div className="collapse-fiability">
         <h3 className="collapse-fiability__title">Fiabilité</h3>
         <FontAwesomeIcon
           icon={faChevronUp}
-          className="collapse-fiability__btn"
+          className={`collapse-fiability__btn ${isOpen ? 'open' : ''}`}
+          onClick={toggleCollapse}
         />
+      </div>
+      <div className={`collapse-fiability__content ${isOpen ? 'open' : ''}`}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi enim
+        neque sint! Repellat dignissimos, dolores iusto eius dolorem excepturi
+        eos quos dolore laborum aperiam repudiandae. Iusto atque amet vitae quo.
       </div>
       <div className="collapse-respect">
         <h3 className="collapse-respect__title">Respect</h3>
