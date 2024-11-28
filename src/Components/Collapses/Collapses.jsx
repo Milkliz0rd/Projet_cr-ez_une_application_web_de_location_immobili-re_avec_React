ea06499c59dac3c0c5f6a220f76f16a925b6a1c8
Morozoff Alexandre <morozoff_alexandre@outlook.com>
2024-11-28 10:55:57 +0100

    page about complete

--- src/Components/Collapses/Collapses.jsx
+++ src/Components/Collapses/Collapses.jsx
@@ -3,43 +3,60 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, { useState } from 'react'
 
 const Collapses = () => {
-  const [isOpen, setIsOpen] = useState(false)
-  function toggleCollapse() {
-    setIsOpen(!isOpen)
+  const [openCollapses, setOpenCollapses] = useState([])
+  const toggleCollapse = (id) => {
+    if (openCollapses.includes(id)) {
+      setOpenCollapses(openCollapses.filter((collapseId) => collapseId !== id))
+    } else {
+      setOpenCollapses([...openCollapses, id])
+    }
   }
+  const collapses = [
+    {
+      id: 1,
+      title: 'Fiabilité',
+      content:
+        'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements et toutes les informations sont régulièrement vérifiées par nos équipes.',
+    },
+    {
+      id: 2,
+      title: 'Respect',
+      content:
+        'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme',
+    },
+    {
+      id: 4,
+      title: 'Service',
+      content:
+        'La qualité du service est au cœur de notre engagement chez Kasa. Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance ',
+    },
+    {
+      id: 5,
+      title: 'Securité',
+      content:
+        "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos service. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standarts sont bien rescpectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+    },
+  ]
   return (
     <div className="collapse-container">
-      <div className="collapse-fiability">
-        <h3 className="collapse-fiability__title">Fiabilité</h3>
-        <FontAwesomeIcon
-          icon={faChevronUp}
-          className={`collapse-fiability__btn ${isOpen ? 'open' : ''}`}
-          onClick={toggleCollapse}
-        />
-      </div>
-      <div className={`collapse-fiability__content ${isOpen ? 'open' : ''}`}>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi enim
-        neque sint! Repellat dignissimos, dolores iusto eius dolorem excepturi
-        eos quos dolore laborum aperiam repudiandae. Iusto atque amet vitae quo.
-      </div>
-      <div className="collapse-respect">
-        <h3 className="collapse-respect__title">Respect</h3>
-        <FontAwesomeIcon icon={faChevronUp} className="collapse-respect__btn" />
-      </div>
-      <div className="collapse-services">
-        <h3 className="collapse-services__title">Services</h3>
-        <FontAwesomeIcon
-          icon={faChevronUp}
-          className="collapse-services__btn"
-        />
-      </div>
-      <div className="collapse-security">
-        <h3 className="collapse-security__title">securité</h3>
-        <FontAwesomeIcon
-          icon={faChevronUp}
-          className="collapse-security__btn"
-        />
-      </div>
+      {collapses.map((collapse) => (
+        <div key={collapse.id} className="collapse">
+          <div className="collapse__header">
+            <h3 className="collapse__header--title">{collapse.title}</h3>
+            <FontAwesomeIcon
+              style={{ height: '32px', width: 'auto' }}
+              icon={faChevronUp}
+              className={`collapse__header--btn ${openCollapses.includes(collapse.id) ? 'open' : ''}`}
+              onClick={() => toggleCollapse(collapse.id)}
+            />
+          </div>
+          <div
+            className={`collapse__content ${openCollapses.includes(collapse.id) ? 'open' : ''}`}
+          >
+            {collapse.content}
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
