5b99066df75dfdb3d0c27cda328fb896104f11b7
Morozoff Alexandre <morozoff_alexandre@outlook.com>
2024-12-10 15:04:30 +0100

    tags are adding

--- src/Components/AccomodationInfos/AccomodationsInfos.jsx
+++ src/Components/AccomodationInfos/AccomodationsInfos.jsx
@@ -1,19 +1,33 @@
 import React from 'react'
 
 const AccomodationsInfos = ({ accomodation }) => {
+  const tags = accomodation.tags
   return (
-    <div className="accomodations-infos">
-      <div className="accomodations-infos__title">
-        <h1 className="accomodations-infos__title--h1">{accomodation.title}</h1>
-        <p className="accomodations-infos__title--p">{accomodation.location}</p>
+    <div>
+      <div className="accomodations-infos-header">
+        <div className="accomodations-infos-header__title">
+          <h1 className="accomodations-infos-header__title--h1">
+            {accomodation.title}
+          </h1>
+          <p className="accomodations-infos-header__title--p">
+            {accomodation.location}
+          </p>
+        </div>
+        <div className="accomodations-infos-header__host">
+          {accomodation.host.name}
+          <img
+            src={accomodation.host.picture}
+            alt={accomodation.host.name}
+            className="accomodations-infos-header__host--img"
+          />
+        </div>
       </div>
-      <div className="accomodations-infos__host">
-        {accomodation.host.name}
-        <img
-          src={accomodation.host.picture}
-          alt={accomodation.host.name}
-          className="accomodations-infos__host--img"
-        />
+      <div className="accomodations-info-body">
+        <ul className="accomodations-info-body__tags">
+          {tags.map((tag) => (
+            <li className="accomodations-info-body__tags--container">{tag}</li>
+          ))}
+        </ul>
       </div>
     </div>
   )
