3a7a9bf1266d60c90c3ae6d25da1ae62cef3c92e
Alexandre Morozoff <morozoff_alexandre@outlook.com>
2024-12-23 18:20:17 +0100

    redirection simply on accomodotion sheet

--- src/Pages/AccomodationSheet/AccommodationSheet.jsx
+++ src/Pages/AccomodationSheet/AccommodationSheet.jsx
@@ -1,21 +1,13 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Carouselle from '../../Components/Carousselle/Carouselle'
-import { useLocation, useNavigate, useParams } from 'react-router-dom'
+import { useLocation, useParams } from 'react-router-dom'
 import AccomodationsInfos from '../../Components/AccomodationInfos/AccomodationsInfos'
 import Error404 from '../Error404/Error404'
 
 const AccomodationSheet = () => {
   const { accomodationId } = useParams() // Récupère l'ID depuis l'URL
-  const navigate = useNavigate() // permet de rediriger ver une autre page
   const { state } = useLocation() // Récupère les données passées via state
   const accomodation = state?.accommodation // Si state est défini, récupère l'objet accommodation
-
-  // Si aucune accommodation n'est trouvée, redirige vers la page 404
-  useEffect(() => {
-    if (!accomodation) {
-      navigate('/404', { replace: true })
-    }
-  }, [accomodation, navigate])
 
   return (
     <div>
